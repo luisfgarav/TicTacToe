c70ce1b345905afc0bd78aa13a32e67233e459e1
Luis Felipe <luisfgaravil@gmail.com>
2018-05-11 02:24:09 -0500

    Acción de botones y cambio de color

--- app/src/main/java/com/example/ppc/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/ppc/tictactoe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ppc.tictactoe;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,25 +12,236 @@ public class MainActivity extends AppCompatActivity {
     public TextView ganador,turnillo;
     public Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9;
     public int turno=1;
-
+    public int casillas1[]=new int[9];
+    public int casillas2[]=new int[9];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bt1=(Button)findViewById(R.id.btn1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press1(view);
+            }
+        });
         bt2=(Button)findViewById(R.id.btn2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press2(view);
+            }
+        });
         bt3=(Button)findViewById(R.id.btn3);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press3(view);
+            }
+        });
         bt4=(Button)findViewById(R.id.btn4);
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press4(view);
+            }
+        });
         bt5=(Button)findViewById(R.id.btn5);
+        bt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press5(view);
+            }
+        });
         bt6=(Button)findViewById(R.id.btn6);
+        bt6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press6(view);
+            }
+        });
         bt7=(Button)findViewById(R.id.btn7);
+        bt7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press7(view);
+            }
+        });
         bt8=(Button)findViewById(R.id.btn8);
+        bt8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press8(view);
+            }
+        });
         bt9=(Button)findViewById(R.id.btn9);
-        ganador=(TextView)findViewById(R.id.winner);
+        bt9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                press9(view);
+            }
+        });
+        ganador=(TextView)findViewById(R.id.Winner);
         turnillo=(TextView)findViewById(R.id.Turno);
+        turnillo.setText("X");
 
+    }
 
+    public void press1(View view){
+        if(casillas1[0]!=1&&casillas2[0]!=1) {
+            if (turno == 1) {
+                bt1.setText("X");
+                bt1.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[0] = 1;
+                turno=2;
+            } else {
+                bt1.setText("O");
+                bt1.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[0] = 1;
+                turno=1;
+            }
+        }
+
+    }
+    public void press2(View view){
+        if(casillas1[1]!=1&&casillas2[1]!=1) {
+            if (turno == 1) {
+                bt2.setText("X");
+                bt2.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[1] = 1;
+                turno=2;
+            } else {
+                bt2.setText("O");
+                bt2.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[1] = 1;
+                turno=1;
+            }
+        }
+    }
+
+    public void press3(View view){
+        if(casillas1[2]!=1&&casillas2[2]!=1) {
+            if (turno == 1) {
+                bt3.setText("X");
+                bt3.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[2] = 1;
+                turno=2;
+            } else {
+                bt3.setText("O");
+                bt3.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[2] = 1;
+                turno=1;
+            }
+        }
+    }
+    public void press4(View view){
+        if(casillas1[3]!=1&&casillas2[3]!=1) {
+            if (turno == 1) {
+                bt4.setText("X");
+                bt4.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[3] = 1;
+                turno=2;
+            } else {
+                bt4.setText("O");
+                bt4.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[3] = 1;
+                turno=1;
+            }
+        }
+    }
+    public void press5(View view){
+        if(casillas1[4]!=1&&casillas2[4]!=1) {
+            if (turno == 1) {
+                bt5.setText("X");
+                bt5.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[4] = 1;
+                turno=2;
+            } else {
+                bt5.setText("O");
+                bt5.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[4] = 1;
+                turno=1;
+            }
+        }
+    }
+    public void press6(View view){
+        if(casillas1[5]!=1&&casillas2[5]!=1) {
+            if (turno == 1) {
+                bt6.setText("X");
+                bt6.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[5] = 1;
+                turno=2;
+            } else {
+                bt6.setText("O");
+                bt6.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[5] = 1;
+                turno=1;
+            }
+        }
+    }
+    public void press7(View view){
+        if(casillas1[6]!=1&&casillas2[6]!=1) {
+            if (turno == 1) {
+                bt7.setText("X");
+                bt7.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[6] = 1;
+                turno=2;
+            } else {
+                bt7.setText("O");
+                bt7.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[6] = 1;
+                turno=1;
+            }
+        }
+    }
+    public void press8(View view){
+        if(casillas1[7]!=1&&casillas2[7]!=1) {
+            if (turno == 1) {
+                bt8.setText("X");
+                bt8.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[7] = 1;
+                turno=2;
+            } else {
+                bt8.setText("O");
+                bt8.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[7] = 1;
+                turno=1;
+            }
+        }
+    }
+    public void press9(View view){
+        if(casillas1[8]!=1&&casillas2[8]!=1) {
+            if (turno == 1) {
+                bt9.setText("X");
+                bt9.setBackgroundColor(Color.parseColor("#F44336"));
+                turnillo.setText("O");
+                casillas1[8] = 1;
+                turno=2;
+            } else {
+                bt9.setText("O");
+                bt9.setBackgroundColor(Color.parseColor("#2196f3"));
+                turnillo.setText("X");
+                casillas2[8] = 1;
+                turno=1;
+            }
+        }
     }
 }
 
